850c22a597e2b05192480a1df3c7c443cf8bc55c
Vonney <davonnejv@gmail.com>
2024-03-10 18:50:12 -0500

    editing text and responsiveness

--- src/components/Beautiful.jsx
+++ src/components/Beautiful.jsx
@@ -11,8 +11,8 @@ export default function Beautiful() {
   return (
     <>
       <section className="bg-white flex flex-col justify-center items-center rounded-xl p-2 md:p-4 max-w-sm md:max-w-xl lg:max-w-2xl text-white absolute top-60 md:top-60 lg:top-58">
-        <div className="text-black font-great text-4xl lg:text-5xl mt-5">
-          Brining Nail Art To Life
+        <div className="text-black font-great text-3xl md:text-4xl lg:text-5xl mt-5">
+          Custom Designs Just For You
         </div>
         <div className="mt-5 text-xl md:text-xl lg:text-3xl text-black">
           Bring your favorite nail art and let me turn your nails into the
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,8 +10,8 @@ export default function Header() {
   return (
     <>
       <header>
-        <h1 className="text-black text-5xl md:text-6xl lg:text-7xl font-great mb-96 text-center mt-10">
-          Juliet's Nail Salon
+        <h1 className="text-black text-5xl md:text-6xl lg:text-6xl font-great mb-96 text-center mt-10">
+          Nails By Juliet
           <div className="text-3xl bg-white p-4 rounded-full flex flex-row lg:flex-row gap-5 md:gap-20 lg:gap-20 justify-center items-center cursor-pointer mt-5">
             {links.map((link) => (
               <Link
